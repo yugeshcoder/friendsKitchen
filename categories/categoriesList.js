68cb08bbe40613ee235cc0a1b8ab3c70f221dcd7
yugeshcoder <yugesh.v.1013@gmail.com>
2024-11-26 20:46:40 +0530

    Updates

--- categories/categoriesList.js
+++ categories/categoriesList.js
@@ -3,32 +3,38 @@ import momos from './image/momos.jpg'
 import pizza from './image/pizza.jpg'
 import sandwich from './image/sandwich.jpg'
 import wrap from './image/wrap.jpg'
+import rice from './image/rice.jpg'
 
 const categoriesObject = [
   {
   name : 'Burger',
   image : burger,
-  path : '/burger'
+  path : '/friendsKitchen/burger'
   }
 ,{
   name : 'Wrap & Fries',
   image : wrap,
-  path : '/wrap'
+  path : '/friendsKitchen/wrap'
  },
  {
   name : 'Pizza',
   image : pizza,
-  path : '/pizza'
+  path : '/friendsKitchen/pizza'
  },
  {
   name : 'sandwich',
   image : sandwich,
-  path : '/sandwich'
+  path : '/friendsKitchen/sandwich'
  },
  {
   name : 'Momos & others',
   image : momos,
-  path : '/momos'
+  path : '/friendsKitchen/momos'
+ },
+ {
+  name : 'Fried Rice',
+  image : rice,
+  path : '/friendsKitchen/rice'
  }
 ];
 
